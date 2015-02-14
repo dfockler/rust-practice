9ed2d09a29445355e7404465ce649bb89e5621c3
Dan Fockler <dfockler13@gmail.com>
2015-02-13 21:32:39 -0700

    tabs to spaces

--- src/main.rs
+++ src/main.rs
@@ -2,35 +2,35 @@ use std::thread::Thread;
 use std::sync::{Arc, Mutex, mpsc};
 
 fn main() {
-	let (tx, rx) = mpsc::channel();
+    let (tx, rx) = mpsc::channel();
 
-	let data = [2, 3, 4, 5, 6, 7];
+    let data = [2, 3, 4, 5, 6, 7];
 
-	for i in 0..data.len() {
-		
-		if i % 2 == 0 { continue; }
-		let (data, tx) = (data.clone(), tx.clone());
+    for i in 0..data.len() {
+        
+        if i % 2 == 0 { continue; }
+        let (data, tx) = (data.clone(), tx.clone());
 
-		Thread::spawn(move || {
-			let a = data[i-1];
-			let b = data[i];
+        Thread::spawn(move || {
+            let a = data[i-1];
+            let b = data[i];
 
-			let answer = a + b;
-			tx.send(answer);
-		});
-	}
+            let answer = a + b;
+            tx.send(answer);
+        });
+    }
 
-	let mut sum = 0;
-	
-	for _ in 0..data.len()/2 {
-		match rx.recv() {
-			Ok(result) => {
-				sum += result;
-				println!("{}", result);
-			},
-			Err(e) => println!("Error: {:?}", e), 
-		}
-	}
+    let mut sum = 0;
+    
+    for _ in 0..data.len()/2 {
+        match rx.recv() {
+            Ok(result) => {
+                sum += result;
+                println!("{}", result);
+            },
+            Err(e) => println!("Error: {:?}", e), 
+        }
+    }
 
-	println!("{:?}", sum);
+    println!("{:?}", sum);
 }
